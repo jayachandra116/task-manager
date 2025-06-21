280deb81cf33d8f1340e985e02640fe3c5156953
JAYA CHANDRA <mjchandra97@gmail.com>
2025-06-21 21:07:21 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/jc/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jc.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
