adb9997c537ffe7937fa3340e55356b260cf91b5
JAYA CHANDRA <mjchandra97@gmail.com>
2025-07-14 23:05:23 +0530

    feature: added input valdation for task APIs (#1)

--- src/main/java/com/jc/task_manager/dto/TaskRequest.java
+++ src/main/java/com/jc/task_manager/dto/TaskRequest.java
@@ -2,15 +2,24 @@ package com.jc.task_manager.dto;
 
 import com.jc.task_manager.model.TaskPriority;
 import com.jc.task_manager.model.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class TaskRequest {
+
+    @NotBlank(message = "Title is required")
     private String title;
+
     private String description;
+
     private TaskStatus status;
+
+    @NotNull(message = "Priority is required")
     private TaskPriority priority;
+    
     private LocalDate dueDate;
 }
